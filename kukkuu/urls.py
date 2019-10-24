be7ca1a6f764e8a6b52e04955c8c597006792697
Tuomas Haapala <tuomas.haapala@anders.fi>
2019-10-24 09:19:48 +0300

    Disable Graphiql in production

--- kukkuu/urls.py
+++ kukkuu/urls.py
@@ -3,7 +3,9 @@ from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 from helusers.admin_site import admin
 
+from kukkuu import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=settings.DEBUG))),
 ]
