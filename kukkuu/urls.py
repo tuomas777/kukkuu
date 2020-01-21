05f992eff9b83c54c4af0399dc9af7fb4ccf65ac
Tuomas Haapala <tuomas.haapala@anders.fi>
2020-01-21 16:29:24 +0200

    Revert "Add max depth for API queries"
    This reverts commit 1e8072be65351d127e26c4988da2971122c4c85f.

--- kukkuu/urls.py
+++ kukkuu/urls.py
@@ -3,22 +3,12 @@ from django.http import HttpResponse
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from helusers.admin_site import admin
-from secure_graphene.depth import DepthAnalysisBackend
 
 from kukkuu.views import SentryGraphQLView
 
-gql_backend = DepthAnalysisBackend(
-    max_depth=settings.KUKKUU_QUERY_MAX_DEPTH, execute_params={"executor": None}
-)
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "graphql",
-        csrf_exempt(
-            SentryGraphQLView.as_view(graphiql=settings.DEBUG, backend=gql_backend)
-        ),
-    ),
+    path("graphql", csrf_exempt(SentryGraphQLView.as_view(graphiql=settings.DEBUG))),
 ]
 
 
