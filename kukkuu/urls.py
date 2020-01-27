979c2c8acb7eba103be4b36fe44ad97e14e18693
Khin Loc <khin.loc@anders.fi>
2020-01-27 16:14:42 +0200

    Add support to upload event image

--- kukkuu/urls.py
+++ kukkuu/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.http import HttpResponse
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
@@ -24,3 +25,5 @@ def readiness(*args, **kwargs):
 
 
 urlpatterns += [path("healthz", healthz), path("readiness", readiness)]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
