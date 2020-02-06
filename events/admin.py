453b2cff0a4674c26da273fe008fadd6c56ef655
Khin Loc <khin.loc@anders.fi>
2020-02-06 14:55:56 +0200

    Update publish action to django admin interface

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 from parler.admin import TranslatableAdmin
 
+from users.models import Guardian
+
 from .models import Event, Occurrence
+from .notifications import NotificationType
+from .utils import send_event_notifications_to_guardians
 
 
 class OccurrencesInline(admin.StackedInline):
@@ -17,14 +22,26 @@ class EventAdmin(TranslatableAdmin):
         "short_description",
         "capacity_per_occurrence",
         "participants_per_invite",
-        "created_at",
-        "updated_at",
+        "is_published",
     )
     list_display_links = ("id", "name")
     exclude = ("id", "created_at", "updated_at", "published_at")
     inlines = [
         OccurrencesInline,
     ]
+    actions = ["publish"]
+
+    def publish(self, request, queryset):
+        # TODO: Send notifications to guardian who belongs to the same project
+        guardians = Guardian.objects.all()
+        for obj in queryset:
+            obj.publish()
+            send_event_notifications_to_guardians(
+                obj, NotificationType.EVENT_PUBLISHED, guardians
+            )
+        self.message_user(request, _("%s successfully published.") % queryset.count())
+
+    publish.short_description = _("Publish selected events")
 
 
 @admin.register(Occurrence)
