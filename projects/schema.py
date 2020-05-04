0eeb449ed6c6adfc6aa5150957b4f038c17b4859
Tuomas Haapala <tuomas.haapala@anders.fi>
2020-05-05 00:26:02 +0300

    Add field name to Project

--- projects/schema.py
+++ projects/schema.py
@@ -1,10 +1,26 @@
+import graphene
+from django.apps import apps
 from graphene import relay
 from graphene_django import DjangoConnectionField, DjangoObjectType
 from graphql_jwt.decorators import login_required
 from projects.models import Project
 
+from users.schema import LanguageEnum
+
+ProjectTranslation = apps.get_model("projects", "ProjectTranslation")
+
+
+class ProjectTranslationType(DjangoObjectType):
+    language_code = LanguageEnum(required=True)
+
+    class Meta:
+        model = ProjectTranslation
+        fields = ("name", "language_code")
+
 
 class ProjectNode(DjangoObjectType):
+    name = graphene.String()
+
     class Meta:
         model = Project
         interfaces = (relay.Node,)
